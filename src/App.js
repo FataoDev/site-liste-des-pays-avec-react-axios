086af2b7d3c15a716a5e178964f3fa43ca1fd615
Fatao OUEDRAOGO <fataoouedraogo226@gmail.com>
2023-04-09 22:31:19 +0000

    Initialisation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-const App=()=>{
-  return <h1>Hello React</h1>
-}
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React</h1>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>A propos</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis laborum veniam, explicabo eum architecto maiores quidem voluptate ad nihil corrupti, sunt suscipit facilis minus, atque inventore odit reprehenderit minima soluta id numquam assumenda! Magnam, vel iure excepturi ex rem nihil corporis dolores quia tenetur deserunt similique odio eaque corrupti debitis eveniet dolorum velit fuga? Dolores, facere! Asperiores distinctio dicta voluptates dolorem, maiores, consectetur debitis ratione ullam omnis dolores reprehenderit fugit! Eligendi non eius ab vel perspiciatis aspernatur? Reiciendis debitis eaque nulla sint porro? Mollitia vero, facilis sint eveniet explicabo dolores! Vero minus est iusto quasi iure deleniti ab a amet?</p>
+        </div>
+    );
+};
+
+export default About;
